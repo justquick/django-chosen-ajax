18270a97782dbca8dc5f056e55e3569d772257c4
Eric Honkanen <dump@epicowl.com>
2013-07-22 08:14:19 -0400

    Added login_required

--- chosen/views.py
+++ chosen/views.py
@@ -13,6 +13,7 @@ class LoginRequiredMixin(object):
     """
     View mixin which requires that the user is authenticated.
     """
+    @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
 
